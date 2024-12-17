f2de34088d5096ae3750647e8d799b87d00bf7f8
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2024-12-17 09:12:08 +0530

    comit

--- server/main.js
+++ server/main.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.json());
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../')));
 
 // Route for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Google Generative AI Setup
